f8cacb217933b80780d4ff95769923b64a5fe0e8
Ali Gasymov <alik0211alik@gmail.com>
2019-03-30 10:19:30 +0400

    Refactor matrix

--- matrix.js
+++ matrix.js
@@ -1,46 +1,52 @@
-function copy(lhs, rhs) {
-  for (let i = 0; i < 2; ++i) {
-    for (let j = 0; j < 2; ++j) {
-      rhs[i][j] = lhs[i][j];
-    }
-  }
+function BinMatrix(a, b) {
+  this.matrix = [a, b];
+
+  return this;
 }
 
-function mul(lhs, rhs) {
+BinMatrix.prototype.pow = function(n) {
+  const res = [[1, 0], [0, 1]];
+
+  while (n) {
+    if (n & 1) {
+      this.mul(res);
+    }
+
+    this.mul(this.matrix);
+    n >>= 1;
+  }
+
+  this.copy(res, this.matrix);
+};
+
+BinMatrix.prototype.mul = function(rhs) {
   const res = [[0, 0], [0, 0]];
 
   for (let i = 0; i < 2; ++i) {
     for (let j = 0; j < 2; ++j) {
       for (let k = 0; k < 2; ++k) {
-        res[i][j] += lhs[i][k] * rhs[k][j];
+        res[i][j] += this.matrix[i][k] * rhs[k][j];
       }
     }
   }
 
-  copy(res, rhs);
-}
+  this.copy(res, rhs);
+};
 
-function pow(mx, n) {
-  const res = [[1, 0], [0, 1]];
-
-  while (n) {
-    if (n & 1) {
-      mul(mx, res);
+BinMatrix.prototype.copy = function(lhs, rhs) {
+  for (let i = 0; i < 2; ++i) {
+    for (let j = 0; j < 2; ++j) {
+      rhs[i][j] = lhs[i][j];
     }
-
-    mul(mx, mx);
-    n >>= 1;
   }
-
-  copy(res, mx);
-}
+};
 
 function F(n) {
-  const base = [[0, 1], [1, 1]];
+  const matrixInstance = new BinMatrix([0, 1], [1, 1]);
 
-  pow(base, n);
+  matrixInstance.pow(n);
 
-  return base[0][1];
+  return matrixInstance.matrix[0][1];
 }
 
 module.exports = F;
